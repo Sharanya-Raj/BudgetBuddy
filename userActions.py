352ffdcab5fb465cac89c162a7b6b63a3c177cfc
VishwaA3 <140357629+VishwaA3@users.noreply.github.com>
2025-11-14 14:16:56 -0500

    Update userActions.py
    added all the functions in userActions.py

---
+++ userActions.py
@@ -0,0 +1,19 @@
+from user import User 
+
+class userActions:
+    def __init__(self, user):
+        self.user = user    
+    def transactionTracker(self): 
+        pass
+    def recordData(dollarlst):
+        pass
+    def initializeUser(user):
+        pass
+    def loginUser(user):
+        pass
+    def totalCalculator(self):
+        pass
+    def connect_with_NJIT_resources(help_category):
+        pass
+    def evaluate_spending(self):
+        pass
