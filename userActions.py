c2feaf9a1418a7735c2344377476217294c26267
garimathapa2248-star <garimathapa2248@gmail.com>
2025-11-14 14:02:43 -0500

    Implement evaluate spending function
    Adds a function to evaluate spending against income.

---
+++ userActions.py
@@ -0,0 +1,15 @@
+def evaluatespending(amount, income):
+    """
+    Evaluates if the spending amount exceeds a given threshold.
+
+    Parameters:
+    amount (float): The spending amount to evaluate.
+    threshold (float): The threshold to compare against.
+
+    Returns:
+    str: A message indicating whether the spending is within the limit or exceeds it.
+    """
+    if amount > income:
+        return "Spending exceeds limit.You need to cut down your expenses."
+    else:
+        return "Spending is within the limit. You are doing good."
