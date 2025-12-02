c80529c8693cc1f914f07681fadc4695be058bd6
VishwaA3 <140357629+VishwaA3@users.noreply.github.com>
2025-12-01 22:41:40 -0500

    Update userActions.py

--- userActions.py
+++ userActions.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from user import User 
+import matplotlib.pyplot as myPlot
 
 class userActions:
     def __init__(self, userName, password, email, studentNJIT, newAcct,help_category):
@@ -16,6 +17,23 @@ class userActions:
         '''Displays all transactions in the given dictionary (revenue/expense).'''
         for transaction in typeDict:
             print(transaction+": $"+str(typeDict[transaction]),end="\n")
+        
+        # If the dictionary is empty, then skip the plotting
+        if len(typeDict) == 0:
+            print("No data for graph.")
+            return 
+        
+        plotLabels = list(typeDict.keys())
+        plotValues = list(typeDict.values())
+
+        myPlot.figure(figsize=(7,4))
+        myPlot.bar(plotLabels, plotValues)
+        myPlot.title("Transaction Overview")
+        myPlot.xlabel("Category")
+        myPlot.yLabel("Amount ($)")
+        myPlot.xticks(rotation = 45)
+        myPlot.tight_layout()
+        myPlot.show()
 
     def recordData(self, typeDict, transactionType, amount):
         '''Records a new transaction by updating the appropriate dictionary.'''
